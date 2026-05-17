646c8945a08842741bac94443af08496f79575a6
Mariana Soares <marianaccsoares@outlook.com>
2026-05-17 18:34:35 -0300

    Java Spring Boot - Exercício 1

---
+++ CadastroLoja/src/test/java/progcomp/Exercicios/CadastroLoja/CadastroLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package progcomp.Exercicios.CadastroLoja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
